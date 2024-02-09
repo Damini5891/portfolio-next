f1b4b4a4431e13edf72e1d2fd80aa3161720cfda
Damini <damini@brain-crowd.com>
2024-02-09 19:36:52 +0530

    mobile hire me

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const HireMe = () => {
   return (
     <div className='fixed left-4 bottom-4 flex items-center justify-center overflow-hidden
-    md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute
+    md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute md:z-20 md:translate-y-[-70%]
     '>
       <div className='w-40 h-auto flex items-center justify-center relative md:w-24'>
 <CircularText className={"fill-dark animate-spin-slow dark:fill-light"} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         <title>Damini Rai | Homepage</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex items-center text-dark w-full min-h-screen dark:text-light">
+      <main className="flex items-center text-dark w-full min-h-screen dark:text-light relative">
         <Layout className="pt-0 md:pt-16 sm:pt-8">
           <div className="flex items-center justify-between w-full lg:flex-col">
             <div className="w-1/2 lg:hidden md:inline-block md:w-full">
